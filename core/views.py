b05a9919f35585e53e13544d7d56f493064b2990
Nehemiah Mekonnen <binyamm83@gmail.com>
2024-11-08 14:40:30 +0300

    superuser and permission implemented

--- core/views.py
+++ core/views.py
@@ -6,11 +6,15 @@ from core.permission import IsSuperUser
 from core.serializers import CompanySerializer
 from core.couch import sanitize_database_name, generate_secure_password, create_couchdb_database, create_couchdb_user, \
     delete_couchdb_database
+import requests
+from uuid import uuid4
+import bcrypt
 
 
 class CompanyViewSet(ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+    permission_classes = [IsAuthenticated, IsSuperUser]
 
     def get_queryset(self):
         return Company.objects.all()
@@ -35,6 +39,8 @@ class CompanyViewSet(ModelViewSet):
 
             if create_couchdb_database(database_name, database_user):
                 create_couchdb_user(database_user, database_password)
+                initialize_permissions(company_instance.database_name)
+                initialize_superuser(company_instance.database_name)
 
     def perform_destroy(self, instance):
         database_name = sanitize_database_name(instance.name)
@@ -50,3 +56,78 @@ class CompanyViewSet(ModelViewSet):
             database_password = company_instance.database_password
             create_couchdb_user(database_user, database_password)
             return company_instance
+
+initial_permissions = [
+    {'name': 'Location', 'type': 'permission', 'is_active': False, 'permission_id': 1},
+    {'name': 'Category', 'type': 'permission', 'is_active': False, 'permission_id': 2},
+    {'name': 'Products', 'type': 'permission', 'is_active': False, 'permission_id': 3},
+    {'name': 'Product_modifier', 'type': 'permission', 'is_active': False, 'permission_id': 4},
+    {'name': 'Discount', 'type': 'permission', 'is_active': False, 'permission_id': 5},
+    {'name': 'Session', 'type': 'permission', 'is_active': False, 'permission_id': 6},
+    {'name': 'User', 'type': 'permission', 'is_active': False, 'permission_id': 7},
+    {'name': 'POS', 'type': 'permission', 'is_active': False, 'permission_id': 8},
+    {'name': 'Order', 'type': 'permission', 'is_active': False, 'permission_id': 9},
+    {'name': 'Refund', 'type': 'permission', 'is_active': False, 'permission_id': 10},
+    {'name': 'Entity_category', 'type': 'permission', 'is_active': False, 'permission_id': 11},
+    {'name': 'Entity', 'type': 'permission', 'is_active': False, 'permission_id': 12},
+    {'name': 'Customer', 'type': 'permission', 'is_active': False, 'permission_id': 13},
+    {'name': 'Account', 'type': 'permission', 'is_active': False, 'permission_id': 14},
+]
+
+
+def initialize_permissions(db_name):
+    couch_db_url = "http://admin:secret@localhost:5984/"
+
+    response = requests.get(f"{couch_db_url}/{db_name}")
+    if response.status_code == 404:
+        response = requests.put(f"{couch_db_url}/{db_name}")
+        if response.status_code != 201:
+            raise Exception("Error creating CouchDB database")
+
+    bulk_data = [
+        {**permission, '_id': f"permission_{uuid4()}"} for permission in initial_permissions
+    ]
+
+    response = requests.post(f"{couch_db_url}/{db_name}/_bulk_docs", json={"docs": bulk_data})
+    if response.status_code != 201:
+        raise Exception(f"Error creating permission documents: {response.text}")
+
+    print("Permissions initialized in CouchDB.")
+
+
+SUPER_USER = {
+    "_id": f"{uuid4()}",
+    "name": "Super User",
+    "is_active": True,
+    "type": "user",
+    "password": "12345",
+}
+
+
+def hash_password(password: str) -> str:
+
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password.decode('utf-8')
+
+
+def initialize_superuser(db_name):
+    couch_db_url = "http://admin:secret@localhost:5984/"
+
+    response = requests.get(f"{couch_db_url}/{db_name}/superuser")
+    if response.status_code == 404:
+        hashed_password = hash_password(SUPER_USER["password"])
+
+        superuser_doc = {
+            "_id": "superuser",
+            **SUPER_USER,
+            "password": hashed_password
+        }
+
+        response = requests.post(f"{couch_db_url}/{db_name}", json=superuser_doc)
+        if response.status_code != 201:
+            raise Exception(f"Error creating superuser document: {response.text}")
+
+        print("Superuser initialized in CouchDB.")
+    else:
+        print("Superuser already exists in CouchDB.")
