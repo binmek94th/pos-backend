5c3032f962a3f86c82aeca3787e58b7ca064b46d
Nehemiah Mekonnen <binyamm83@gmail.com>
2024-11-14 19:10:09 +0300

    setting edited for production

--- posBackend/settings.py
+++ posBackend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a+yx&5qj87_(@$#zy&5s*#futs@-oh)r=q-x+e78z5-==2y+mi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['206.189.246.185']
 
 
 # Application definition
